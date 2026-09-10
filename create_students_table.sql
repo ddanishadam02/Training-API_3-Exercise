bcdcac3865c79161721e3cf4e652f656d9c73706
ddanishadam02 <ddanishadam02@gmail.com>
2026-09-10 15:01:18 +0800

    Delete create_students_table.sql

--- create_students_table.sql
+++
@@ -1,8 +0,0 @@
--- Run this in Supabase: Dashboard > SQL Editor > New query
-
-create table if not exists students (
-    id bigint generated always as identity primary key,
-    name text not null,
-    age integer not null,
-    grade text not null
-);
